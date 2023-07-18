6c401fb0baf760edcace719211b079488af3eb02
matt_carmody <matt.carmody@ovsgroup.com>
2023-07-17 22:28:23 -0500

    add instructions

---
+++ final_project.py
@@ -0,0 +1,32 @@
+
+
+"""
+
+In this project, you can choose/design any real world application or data analysis using Python
+programming.
+Alternatively, you can choose one problem from the exercises in Chapter 4 or any chapter after Chapter 4
+of the textbook. This textbook has a rich collection of programming exercises, with varying complexities
+and difficulties. Many problems have labels like
+Challenge
+Project
+Challenge Project
+Super Challenge Project
+Research
+Performance Analysis
+Intro to Data Science
+AI Project
+etc.
+You can pick any problem. Make sure you choose a project that is challenging enough for your intelligence
+capacity but also simple enough for the short period of a summer course
+
+What to Submit:
+(1) A Project Report
+If you are to choose/design your own application, you need to describe your problems and solutions in
+detail. If you are to choose one in the textbook, you need to copy the problem description, and where the
+problem is located (chapter, problem number and page number).
+(2) The Code
+(i)The Jupyter Notebook files (.ijpnb) files, if any. It is alright if you do not use Jupyter Notebook.
+(ii) The Python script files.
+Do not zip the files. Submit these files separately.
+
+"""
