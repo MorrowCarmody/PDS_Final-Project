f81c24c0ec6a9e6e9113fc3c8eaac9a58c7295ee
matt_carmody <matt.carmody@ovsgroup.com>
2023-07-18 02:08:54 -0500

    add messy start code

--- final_project.py
+++ final_project.py
@@ -1,7 +1,78 @@
+# Matt Carmody
+# COSC 6375
+# Final Project
+
+gb = [[' ' for x in range(3)] for y in range(3)]
+
+# gb = [['1','2','3'],
+#       ['4','5','6'],
+#       ['7','8','9']]
+# gb = [[' ',' ',' '],
+#         [' ',' ',' '],
+#         [' ',' ',' ']]
+
+def resetGame(arr):
+    for i in arr:
+        for j in i:
+            j = ' '
+
+print(gb)
+resetGame(gb)
+print(gb)
+    
+# TODO: Refactor this
+def check_gb():
+    if(gb[0][0] == gb[0][1] == gb[0][2] or # horizontal
+       gb[1][0] == gb[1][1] == gb[1][2] or # horizontal
+       gb[2][0] == gb[2][1] == gb[2][2] or # horizontal
+       gb[0][0] == gb[1][0] == gb[2][0] or # vertical
+       gb[0][1] == gb[1][1] == gb[2][1] or # vertical
+       gb[0][3] == gb[1][3] == gb[2][3] or # vertical
+       gb[0][0] == gb[1][1] == gb[2][2] or # diagonal
+       gb[0][2] == gb[1][1] == gb[2][0]):  # diagonal
+        print("GAME OVER!")
+    
+def playGame():
+    finished = False
+    while(finished == False):
+          print("not finished")
+          finished = True
+    # show board
+    # get user input
+    # choose X or O
+    # loop: while(running)
+        # choose move
+        # change players
+        # if game finishes, end and announce winner
+          
+
+def printGame(arr):
+    for i in arr:
+        for j in i:
+            print(j,' ', end=' ')
+        print('\n')
+    # HARDCODE BOARD?
+    # print(' ',gb[0][0],'|',gb[0][1],'|',gb[0][2])
+    # print('-------------')
+    # print(' ',gb[1][0],'|',gb[1][1],'|',gb[1][2])
+    # print('-------------')
+    # print(' ',gb[2][0],'|',gb[2][1],'|',gb[2][2])
+
+printGame(gb)
+
+"""
+(Project: Two-Player, Two-Dimensional Tic-Tac-Toe) Write a script to play two-
+dimensional Tic-Tac-Toe between two human players who alternate entering their moves
+on the same computer. Use a 3-by-3 two-dimensional array. Each player indicates their
+moves by entering a pair of numbers representing the row and column indices of the
+square in which they want to place their mark, either an 'X' or an 'O'. When the first play-
+er moves, place an 'X' in the specified square. When the second player moves, place an
+'O' in the specified square. Each move must be to an empty square. After each move, de-
+termine whether the game has been won and whether it’s a draw.
+"""
 
 
 """
-
 In this project, you can choose/design any real world application or data analysis using Python
 programming.
 Alternatively, you can choose one problem from the exercises in Chapter 4 or any chapter after Chapter 4
@@ -28,5 +99,17 @@ problem is located (chapter, problem number and page number).
 (i)The Jupyter Notebook files (.ijpnb) files, if any. It is alright if you do not use Jupyter Notebook.
 (ii) The Python script files.
 Do not zip the files. Submit these files separately.
+"""
 
+
+"""
+6.8 (Challenge: Writing the Word Equivalent of a Check Amount) In check-writing
+systems, it’s crucial to prevent alteration of check amounts. One common security method
+requires that the amount be written in numbers and spelled out in words as well. Even if
+someone can alter the numerical amount of the check, it’s tough to change the amount in
+words. Create a dictionary that maps numbers to their corresponding word equivalents.
+Write a script that inputs a numeric check amount that’s less than 1000 and uses the dic-
+tionary to write the word equivalent of the amount. For example, the amount 112.43
+should be written as
+ONE HUNDRED TWELVE AND 43/100
 """
